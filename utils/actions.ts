630ef7a610b873d2acc5cb27020dd282dc5a1303
Irfan Baharuddin <irfannaqieb@gmail.com>
2024-09-29 16:41:29 +0900

    Implement NavSearch

--- utils/actions.ts
+++ utils/actions.ts
@@ -9,8 +9,15 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({ search = "" }: { search: string }) => {
   return db.product.findMany({
+    where: {
+      // Using OR condition to match either name or company fields
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { company: { contains: search, mode: "insensitive" } },
+      ],
+    },
     orderBy: {
       createdAt: "desc",
     },
